95b7e6ec4a933d2fb032d9a14b1222f462a78a7d
Xue Feng <530606349@qq.com>
2018-11-06 16:23:28 +0000

    test add new purchaser

--- test/routes/purchaser_test.js
+++ test/routes/purchaser_test.js
@@ -18,4 +18,27 @@ describe('purchaser', function () {
              .expect(200,done);
      });    
  });
+ describe('POST /purchaser', function () {
+    let purchaser = { 
+        PurchaserName: 'mike',
+        P_flowers:'rose',
+        funds:1000
+   };
+    it('should return confirmation message and update ', function (done) {       
+         request(app)
+         .post('/purchaser')
+         .set('Accept', 'application/json')
+         .send(purchaser)
+         .end(function(err, res) {
+             if(!err){
+             expect(res).to.have.status(200);
+             expect(res.body).to.have.property('message').equal('success',purchaser ); 
+             done();
+             }
+             else{
+                expect(res.body).to.have.property('message').equal('this order NOT Added!',err );
+             }
+  });
+});
+});
 });
